6d9378524a7ef79c57662e063267c270fbf05b1a
dwalter <dwalter@ae6b41b2-966d-11dd-bc09-2f8cc32af589>
2009-06-15 06:32:22 +0000

    Delete prawnto

--- vendor/plugins/prawnto/init.rb
+++
@@ -1,6 +0,0 @@
-require 'prawnto'
-
-Mime::Type.register "application/pdf", :pdf
-ActionView::Template.register_template_handler 'prawn', Prawnto::TemplateHandler::Base
-ActionView::Template.register_template_handler 'prawnx', Prawnto::TemplateHandler::Raw  # experimental
-
